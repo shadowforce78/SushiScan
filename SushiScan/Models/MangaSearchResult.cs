d4e0e01881dfa0446c197efed1566214cc79dd64
SaumonDeLuxe <planque.adam@gmail.com>
2025-05-26 16:04:31 +0200

    Refactor MangaSearchResult to make ImageUrl nullable; update HomeView to improve layout and interaction for manga details.

--- SushiScan/Models/MangaSearchResult.cs
+++ SushiScan/Models/MangaSearchResult.cs
@@ -3,15 +3,14 @@ using System.Collections.Generic;
 using Avalonia.Media.Imaging;
 
 namespace SushiScan.Models
-{
-    public class MangaSearchResult
+{    public class MangaSearchResult
     {
         public required string _id { get; set; }
         public required string Title { get; set; }
         public List<string> Genres { get; set; } = new List<string>();
         public required string Type { get; set; }
           // Propriétés supplémentaires pour la gestion des images dans l'application
-        public required string ImageUrl { get; set; }
+        public string? ImageUrl { get; set; }
         public Bitmap? Image { get; set; }
     }
 }
