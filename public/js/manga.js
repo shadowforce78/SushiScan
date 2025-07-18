57f394388bc67e315cf5fecffd49514d99f40fd3
shadowforce78 <planque.adam@gmail.com>
2025-07-18 15:46:56 +0200

    👌 IMPROVE: Update manga page: change language to French, enhance getInfo function for URL encoding, and improve manga info display with cover image and genres

--- public/js/manga.js
+++ public/js/manga.js
@@ -1,9 +1,9 @@
 const API_URL = "https://api.saumondeluxe.com";
 const mangaName = new URLSearchParams(window.location.search).get("slug");
 
-async function getInfo(mangaName){
-    const endpoint = `${API_URL}/scans/manga/info?manga_name=${mangaName}`;
-
+async function getInfo(mangaName) {
+    const endpoint = `${API_URL}/scans/manga/info?manga_name=${encodeURIComponent(mangaName)}`;
+    console.info('Fetching manga info from:', endpoint);
     try {
         const response = await fetch(endpoint);
         if (!response.ok) {
@@ -19,9 +19,20 @@ async function getInfo(mangaName){
 
 window.addEventListener('DOMContentLoaded', async () => {
     const mangaInfo = await getInfo(mangaName);
+    const genres = mangaInfo.manga.genres.map(genre => `<span class="genre">${genre}</span>`).join(', ');
+
+
+    const scansType = []
+    for (const type of mangaInfo.manga.scan_chapters) {
+        scansType.push(`<button class="scan-type" onclick="getScan('${type.name}')">${type.name} : ${type.chapters_count} chapitres disponibles</button>`);
+    }
+
     if (mangaInfo) {
         document.querySelector('.main').innerHTML = `
-            <h1>${mangaInfo.manga.title}</h1>
+            <img src="${mangaInfo.manga.image_url}" alt="${mangaInfo.manga.title} cover" class="cover">
+            <p class="title">${mangaInfo.manga.title}</p>
+            <div class="genres">${genres}</div>
+            <div class="scan-types">${scansType.join(' ')}</div>
         `;
     } else {
         document.querySelector('.main').innerHTML = '<p>Error loading manga information.</p>';
