3dd6096202f06b63f4dafca5295d4bfb209ee2b1
SaumonDeLuxe <planque.adam@gmail.com>
2025-07-21 11:46:28 +0200

    👌 IMPROVE: Enhance chapter navigation in reader.js with previous and next chapter links

--- public/js/reader.js
+++ public/js/reader.js
@@ -18,7 +18,28 @@ async function fetchScanData(mangaName, chapter, scanType) {
     }
 }
 
-function createHeader() {
+async function getMaxChapter(mangaName, scanType) {
+    try {
+        const response = await fetch(`${API_URL}/scans/manga/info/?manga_name=${decodeURIComponent(mangaName)}`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const mangaInfo = await response.json();
+        const scanData = mangaInfo.manga.scan_chapters;
+        if (scanData && scanData.length > 0) {
+            const scan = scanData.find(s => s.name.toLowerCase() === scanType.toLowerCase());
+            if (scan) {
+                return await scan.chapters_count;
+            }
+        }
+        throw new Error('Scan type not found or no chapters available');
+    } catch (error) {
+        console.error('Error fetching scan data:', error);
+        return null;
+    }
+}
+
+async function createHeader() {
     const header = document.createElement('header');
     header.classList.add('header');
 
@@ -34,9 +55,33 @@ function createHeader() {
     backLink.textContent = 'Retour à la liste des chapitres';
     nav.appendChild(backLink);
 
-    header.appendChild(nav);
+    // Navigate to the previous chapter or the next chapter
+    if (parseInt(chapter) <= 1) {
+        // Does not exist
+        const prevLink = document.createElement('span');
+        prevLink.textContent = 'Chapitre Précédent (Non disponible)';
+        nav.appendChild(prevLink);
+    } else {
+        const prevLink = document.createElement('a');
+        prevLink.href = `../html/reader.html?manga=${encodeURIComponent(mangaName)}&chapter=${encodeURIComponent(parseInt(chapter) - 1)}&scan=${encodeURIComponent(scanType)}`;
+        prevLink.textContent = 'Chapitre Précédent';
+        nav.appendChild(prevLink);
+    }
 
-    return header;
+    if (chapter >= await getMaxChapter(mangaName, scanType)) {
+        // Does not exist
+        const nextLink = document.createElement('span');
+        nextLink.textContent = 'Chapitre Suivant (Non disponible)';
+        nav.appendChild(nextLink);
+    } else {
+        const nextLink = document.createElement('a');
+        nextLink.href = `../html/reader.html?manga=${encodeURIComponent(mangaName)}&chapter=${encodeURIComponent(parseInt(chapter) + 1)}&scan=${encodeURIComponent(scanType)}`;
+        nextLink.textContent = 'Chapitre Suivant';
+        nav.appendChild(nextLink);
+
+    }
+    header.appendChild(nav);
+    return await header;
 }
 
 function displayImgs(scanData) {
@@ -65,6 +110,6 @@ function displayImgs(scanData) {
 
 window.addEventListener('DOMContentLoaded', async () => {
     const scanData = await fetchScanData(mangaName, chapter, scanType);
-    document.querySelector('.main').appendChild(createHeader());
+    document.querySelector('.main').appendChild(await createHeader());
     displayImgs(scanData);
 });
